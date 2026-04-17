a831d8bc5412d82b2f542edff915dabe14a5b719
Faris Jamal <111167160+farisjamal@users.noreply.github.com>
2026-04-18 04:29:54 +0800

    update: resume link

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -82,7 +82,7 @@ const HeroSection = () => {
               </div>
               <div className="mt-8 flex flex-col gap-3 w-fit">
                 <Link
-                  href={"https://drive.google.com/file/d/11sdTq76e2H3M6pA2LgtFCt-6jTky3URG/view?usp=drivesdk"}
+                  href={"https://drive.google.com/file/d/19rxy99WFcZH4bu2FYaNeDs1sfZFaBj6f/view?usp=drive_link"}
                   target="_blank"
                   className="flex-1"
                 >
